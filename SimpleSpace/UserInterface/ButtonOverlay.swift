11340b4d8bf0aefd1cf55964a4d760807078e311
Dave-Ed-Cast <148765897+Dave-Ed-Cast@users.noreply.github.com>
2024-12-18 10:54:28 +0100

    Adjusted the button overlay

--- SimpleSpace/UserInterface/ButtonOverlay.swift
+++ SimpleSpace/UserInterface/ButtonOverlay.swift
@@ -11,19 +11,25 @@ struct ButtonOverlay: View {
     
     @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
     @Environment(\.openWindow) private var openWindow
+    @Environment(\.dismissWindow) private var dismissWindow
+    
+    let contentViewID = "ContentView"
+    let buttonOverlayID = "ButtonOverlay"
     
     var body: some View {
         
         VStack(spacing: 20) {
             VStack {
                 Text("Feeling overwhelmed?").font(.title)
-                Text("Consider going back to reality").font(.subheadline)
+                Text("Consider going back to reality.").font(.subheadline)
+                Text("Click the button or \(Text("snap").fontWeight(.bold)) your fingers").font(.subheadline)
             }
+            .multilineTextAlignment(.center)
             .padding()
+            
             Button {
-                
+                dismissWindow(id: buttonOverlayID)
                 Task { await dismissImmersiveSpace() }
-                openWindow(id: "ContentView")
                 
             } label: {
                 
